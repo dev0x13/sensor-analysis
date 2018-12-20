529b9dbeac47ad98b577eeea9fad561c6ee8c3bd
Georgiy M <george.manuilov@gmail.com>
2018-12-21 02:31:13 +0300

    Merged Android debugger app

--- android_debugger_app/app/src/main/java/com/example/mister_programmister/sensorinformator/Analysis.java
+++ android_debugger_app/app/src/main/java/com/example/mister_programmister/sensorinformator/Analysis.java
@@ -9,11 +9,12 @@ public class Analysis {
     }
 
     private enum UserState {
-        stand, walk, sleep
+        stand, walk, sleep, sit
     }
 
-    private boolean chatting;
+    private boolean typing;
     private boolean onStreet;
+    private boolean speaking;
     private int floor;
 
     private StringBuilder sb = new StringBuilder();
@@ -29,18 +30,26 @@ public class Analysis {
     private final float posE = 0.03f;
     private final float lightE = 3f;
 
+    private final float speakTangLimts[] ={1.2f, 1.75f};
+    private final float speakKrenLimts[] ={0.2f, 1.0f};
+
+    public float eulerAngels[];
+
     public final String sLight = "light";
     public final String sRot = "Rotation";
     public final String sDisplay = "Display";
     public final String sSteps = "StepCounter";
     public final String sPressure = "Pressure";
-    public final String sAccel = "Accelerometr";
+    public final String sLinAccel = "linear Accelerometr";
+    public final String sProxim = "Proximity";
+    public final String sMagnit = "Magnetic Field";
 
     private Position currPos = Position.inHand;
     private UserState currState = UserState.stand;
 
     private float accelTrack[];
-    public final float accelTrackCoeffs[] = {1.f, 0.35f, 0.15f};
+    private final float accelTrackCoeffs[] = {1.f, 0.35f, 0.15f};
+    public int typingCount = 0;
     /**
      *
      * @param period - time interval in milliseconds at which data is received
@@ -49,9 +58,19 @@ public class Analysis {
         this.period = period;
         timeStep = (float) period  / 1000.f;
         accelTrack = new float[3];
+        eulerAngels = new float[3];
+        typing = false;
     }
 
     private void updateInfo(Map<String, float[]> data){
+
+        float q[] = new float[4];
+        for(int i = 0; i < 3; i++) {
+            q[i+1] = data.get(sRot)[i];
+        }
+        q[0] = data.get(sRot)[3];
+        quatToEuler(q);
+
         if(Math.pow(Math.pow(data.get(sRot)[0],2) + Math.pow(data.get(sRot)[1],2),0.5) < posE) {
             currPos = Position.onTable;
             startSleepIter = iterations;
@@ -73,8 +92,12 @@ public class Analysis {
             startWalkIter = iterations;
         }
         else {
-            if ((iterations - startWalkIter) * timeStep > 3.5f)
-                currState = UserState.stand;
+            if ((iterations - startWalkIter) * timeStep > 3.5f) {
+                if(eulerAngels[0] > 0.165)
+                    currState = UserState.stand;
+                else
+                    currState = UserState.sit;
+            }
 
             if ((iterations - startSleepIter) * timeStep > 3600.f) { // 3 hours
                 currState = UserState.sleep;
@@ -82,17 +105,39 @@ public class Analysis {
         }
 
 
-        accelTrack[(int)(iterations % 3)] = (float)Math.sqrt(data.get(sAccel)[1] * data.get(sAccel)[1] +
-                data.get(sAccel)[2] * data.get(sAccel)[2]);
+        accelTrack[(int)(iterations % 3)] =
+                (float)Math.sqrt(data.get(sLinAccel)[1] * data.get(sLinAccel)[1] +
+                            data.get(sLinAccel)[2] * data.get(sLinAccel)[2] +
+                            data.get(sLinAccel)[0] * data.get(sLinAccel)[0]);
+
         float accelZAver = (accelTrackCoeffs[0] * accelTrack[(int)(iterations % 3)] +
                             accelTrackCoeffs[1] * accelTrack[(int)((iterations - 1) % 3)] +
                             accelTrackCoeffs[1] *accelTrack[(int)((iterations - 2) % 3)]) /
                 (accelTrackCoeffs[0] +  accelTrackCoeffs[1] +  accelTrackCoeffs[2]);
 
-        chatting = false;
-        if (accelZAver < 0.3f && accelZAver > 0.063f && data.get(sAccel)[0] < 1.f
-                && data.get(sAccel)[1] < 0.7f && data.get(sAccel)[2] < 0.6f)
-            chatting = true;
+
+        if (accelZAver < 1.4f && accelZAver > 0.45f)
+        {
+            if(typingCount < 10)
+            typingCount += 3;
+        }
+        else
+        {
+            if(typingCount > 0 )
+                typingCount -= 2;
+        }
+
+        if(typingCount >= 6 && !typing)
+        {
+            typingCount++;
+            typing = true;
+        }
+
+        if(typingCount < 4 || data.get(sDisplay)[0] == 0.f)
+            typing = false;
+
+
+
 
         if(data.get(sDisplay)[0] == 0.1f)
             startSleepIter  = iterations;
@@ -100,9 +145,16 @@ public class Analysis {
         if(data.get(sLight)[0] < 300.f)
             onStreet = false;
 
-        floor = (int)Math.floor((18.4f * 1000 * Math.log10(770.f / (data.get(sPressure)[0] * 0.750062f)) - 20) / 5);
+        floor = (int)Math.floor((18.4f * 1000 * Math.log10(768.f / (data.get(sPressure)[0] * 0.750062f)) - 20) / 5);
 
         currStepCount = data.get(sSteps)[0];
+
+        speaking = false;
+        if(data.get(sProxim)[0] < 1 &&
+                eulerAngels[0] > speakTangLimts[0] && eulerAngels[0] < speakTangLimts[1] &&
+                ((eulerAngels[1] > speakKrenLimts[0] && eulerAngels[1] < speakKrenLimts[1]) ||
+                (eulerAngels[1] > -speakKrenLimts[1] && eulerAngels[1] < -speakKrenLimts[0])))
+            speaking = true;
     }
 
     public String getInfo(Map<String, float[]> data){
@@ -136,19 +188,45 @@ public class Analysis {
             case stand:
                 userStateInfo += "User is standing";
                 break;
+            case sit:
+                userStateInfo += "User is sitting";
+                break;
         }
 
         if(onStreet){
             userStateInfo += "on the street";
         } else {
+            userStateInfo += " in the building on";
             if(floor >= 1)
-                userStateInfo += " in the building on " + floor + " floor";
+                userStateInfo += " " + floor + " floor";
         }
 
-        if (chatting)
+        if (typing)
             userStateInfo += "\nUser is typing";
+
+        if (speaking)
+            userStateInfo += "\nUser is talking";
 
         sb.append(userStateInfo);
         return sb.toString();
+    }
+
+    private void quatToEuler(float[] q){
+        // roll (x-axis rotation)
+        double sinr_cosp = +2.0 * (q[1] * q[1] + q[2] * q[3]);
+        double cosr_cosp = +1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2]);
+        eulerAngels[0] = (float)Math.atan2(sinr_cosp, cosr_cosp);
+
+        // pitch (y-axis rotation)
+        double sinp = +2.0 * (q[0] * q[2] - q[3] * q[1]);
+        if (Math.abs(sinp) >= 1)
+            eulerAngels[1] = (float)Math.copySign(Math.PI / 2, sinp); // use 90 degrees if out of range
+        else
+            eulerAngels[1] = (float)Math.asin(sinp);
+
+        // yaw (z-axis rotation)
+        double siny_cosp = +2.0 * (q[0] * q[3] + q[1] * q[2]);
+        double cosy_cosp = +1.0 - 2.0 * (q[2] * q[2] + q[3] * q[3]);
+        eulerAngels[2] = (float)Math.atan2(siny_cosp, cosy_cosp);
     }
 }
